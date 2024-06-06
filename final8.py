58528635abc7b338239f7db6c818eea072d800d3
RajaSekharReddy <136687930+RajasekharReddy2370@users.noreply.github.com>
2024-06-06 16:14:05 +0530

    Add files via upload

--- final8.py
+++ final8.py
@@ -20,7 +20,7 @@ if last_id is None:
     last_id = 0
 
 # Get list of Excel files in folder
-folder_path = r"C:\Users\jaswa\Desktop\TS\Telangana_Raw_data\Karimnagar"
+folder_path = r"C:\Users\jaswa\Desktop\AP\Andhra Pradesh_Raw_Data_Folders\Vizainagaram"
 excel_files = [file for file in os.listdir(folder_path) if file.endswith('.xlsx')]
 
 # Read the first Excel file to define table columns
@@ -28,6 +28,7 @@ first_excel_file = excel_files[0]
 first_df = pd.read_excel(os.path.join(folder_path, first_excel_file))
 # first_df["ID"] = ''
 # first_df["State"] = ''
+
 # first_df.insert(0, 'ID', 0)
 # first_df.insert(1, 'State', '')
 
@@ -52,7 +53,7 @@ for excel_file in excel_files:
     last_id += len(df)  # Update last_id to the last ID used in this iteration
 
     # Add State column with value 'Telangana' at first position
-    df.insert(1, 'State', 'Telangana')
+    df.insert(1, 'State', 'AndhraPradesh')
 
     # Iterate through DataFrame and insert rows into MySQL table
     for _, row in df.iterrows():
